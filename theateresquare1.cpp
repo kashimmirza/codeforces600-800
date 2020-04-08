7611b62c88e21d67579aba971ee8bf7f36fb4a4d
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2020-04-08 10:34:53 +0600

    codeforces

---
+++ theateresquare1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int a,b,c;
+ cin>>a>>b>>c;
+ double l=ceil(double(a)/c);
+  double m=ceil(double(b)/c);
+  long long r=l*m;
+  cout<<r<<endl;
+
+
+
+
+
+
+}
